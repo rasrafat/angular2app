30db7a785c3b4a7f4f0fc6ddbfeb32c741ffc4e0
Rafat Rashid <enemyunited@gmail.com>
2017-04-02 02:12:50 -0400

    prune rxjs (#4)

--- src/app/common/services/api.service.ts
+++ src/app/common/services/api.service.ts
@@ -2,9 +2,6 @@ import { Injectable }     from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
 
-// Add the RxJS Observable operators.
-import '../rxjs-operators';
-
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
--- src/vendor.ts
+++ src/vendor.ts
@@ -7,4 +7,13 @@ import '@angular/http';
 import '@angular/router';
 
 // RxJS
-import 'rxjs';
+// See node_module/rxjs/Rxjs.js
+// Import just the rxjs statics and operators needed for THIS app.
+
+// Statics
+import 'rxjs/add/observable/throw';
+
+// operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
